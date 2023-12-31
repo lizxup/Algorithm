311b38fe9dd3d05212bee9268b2a079fab127b7e
Uto_peanut <lizxup@163.com>
2023-12-31 20:29:25 +0800

    初始化

---
+++ src/test/java/TestEverCase.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+import src.binarysearch.BinarySearch;
+
+import java.util.Arrays;
+
+public class TestEverCase {
+
+
+    @Test
+    public void test1(){
+        int[] nums = new int[]{2,5,8};
+        int target = 1;
+        Arrays.sort(nums);
+        int index = Arrays.binarySearch(nums, target);
+        System.out.println(index);
+    }
+
+
+    @Test
+    public void testFarLeft(){
+        int[] nums = new int[]{2,2};
+        int index = BinarySearch.binarySearchFarLeft(nums,3);
+        System.out.println(index);
+    }
+    @Test
+    public void testFarRight(){
+        int[] nums = new int[]{2,2};
+        int index = BinarySearch.binarySearchFarRight(nums,3);
+        System.out.println(index);
+    }
+}
